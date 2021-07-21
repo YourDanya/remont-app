45e0df78f498678baf253e46d79cbbb3b3eb8933
Danyl <pochtaformumumu@gmail.com>
2021-07-21 04:03:55 +0300

    adopt 1

--- src/components/Warranties/Warranties.jsx
+++ src/components/Warranties/Warranties.jsx
@@ -5,26 +5,28 @@ import img from "./warr_img.jpg"
 import {FaPencilAlt, FaRuler, GiInjustice} from "react-icons/all";
 
 const Warranties= () =>{
-    return <div className={styles.warranties}>
-        <div>
-            <div className={styles.title}>Гарантии</div>
+    return <div >
+        <div className={styles.warranties+' '+styles.container}>
             <div className={styles.content}>
-                <div className={styles.item}>
-                    <FaRuler className={styles.icon}/>
-                    Бесплатный <br/>замер
-                </div>
-                <div className={styles.item}>
-                    <GiInjustice className={styles.icon}/>
-                    Гарантия <br/> 3 года
-                </div>
-                <div className={styles.item}>
-                    <FaPencilAlt className={styles.icon}/>
-                    Смета
+                <div className={styles.title}>Гарантии</div>
+                <div className={styles.warrantiesText}>
+                    <div className={styles.item}>
+                        <FaRuler className={styles.icon}/>
+                        Бесплатный <br/>замер
+                    </div>
+                    <div className={styles.item}>
+                        <GiInjustice className={styles.icon}/>
+                        Гарантия <br/> 3 года
+                    </div>
+                    <div className={styles.item}>
+                        <FaPencilAlt className={styles.icon}/>
+                        Смета
+                    </div>
                 </div>
             </div>
-            <div className={styles.abs}></div>
+            <img src={img} className={styles.img} alt={"img"}/>
         </div>
-        <img className={styles.img} src={img}/>
+        <div className={styles.abs}></div>
     </div>
 }
 
