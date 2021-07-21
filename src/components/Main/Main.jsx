f67a314552f91d756feefd50347ef2559f86eaec
Vadim <vadimbaranivsky83@gmail.com>
2021-07-21 12:41:14 +0300

    main adaptive is ready; images need to be cut

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,14 +11,9 @@ const Main = () => {
     const imgRef = React.useRef()
 
     useEffect(() => {
-        console.log(imgRef.current.offsetRight)
-        imgContainerRef.current.scrollTo((imgContainerRef.current.scrollWidth)/2, 0)
-    })
-
-    useEffect(() => {
         const interval = setInterval(() => {
             index >= images.length - 1 ? setIndex(0) : setIndex(index + 1)
-        }, 10000)
+        }, 3000)
         return () => clearInterval(interval)
     }, [index])
 
@@ -26,6 +21,7 @@ const Main = () => {
         <div className='main'>
             <div className="img-container" ref={imgContainerRef}>
               <img src={images[index]} alt="" className='main-img' ref={imgRef}/>
+              <img src={images[index > 0 ? index - 1 : images.length - 1]} key={index} alt="" className='main-img fade-out'/>
             </div>
             <div className="title-container">
                 <div className='title'>cкидка 10% <span>на ремонт квартирі</span></div>        
